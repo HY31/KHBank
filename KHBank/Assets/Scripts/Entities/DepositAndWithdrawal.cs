4c034b2ef80c834b70fa5fe99093a86ac635d8e4
HY31 <qtghdus@gmail.com>
2023-09-20 19:13:48 +0900

    UIController 추가
    MoneyManager를 추가하여 UIController와 연계, 보유 금액과 잔액을 표시할 수 있게 함

---
+++ KHBank/Assets/Scripts/Entities/DepositAndWithdrawal.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DepositAndWithdrawal : MonoBehaviour
+{
+    MoneyManager _moneyManager;
+
+    private void Start()
+    {
+        _moneyManager = MoneyManager.instance;
+    }
+    public void DepositMoney()
+    {
+
+    }
+
+
+}
